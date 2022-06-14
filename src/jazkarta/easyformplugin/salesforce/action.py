c62ad45f91a22b7a150a52ca5cde735eff17ef4c
David Glick <david@glicksoftware.com>
2022-06-14 13:07:21 -0700

    implement basic create operation

--- src/jazkarta/easyformplugin/salesforce/action.py
+++ src/jazkarta/easyformplugin/salesforce/action.py
@@ -1,7 +1,11 @@
 import logging
 
 from collective.easyform.actions import Action, ActionFactory
+from collective.easyform.api import get_context, get_expression
+from DateTime import DateTime
 from plone.supermodel.exportimport import BaseHandler
+from Products.CMFCore.Expression import Expression, getExprContext
+from Products.CMFCore.utils import getToolByName
 from zope.interface import implementer
 
 from . import _
@@ -18,8 +22,56 @@ class SendToSalesforce(Action):
             setattr(self, name, kw.pop(name, field.default))
         super(SendToSalesforce, self).__init__(**kw)
 
+    def get_form(self):
+        return get_context(self)
+
     def onSuccess(self, fields, request):
-        logger.info(fields)
+        form = self.get_form()
+        expr_context = getExprContext(form, form)
+        sf = getToolByName(form, "portal_salesforcebaseconnector")
+
+        for operation in self.operations:
+            sobject = operation["sobject"]
+            data = self.prepare_salesforce_data(sobject, operation["fields"], fields, expr_context)
+            import pdb; pdb.set_trace()
+            op_name = operation["operation"]
+            if op_name == "create":
+                result = sf.create(data)[0]
+                if result["success"]:
+                    sf_id = result["id"]
+                    logger.info(u"Created {} {} in Salesforce".format(sobject, sf_id))
+                else:
+                    err = result["errors"][0]["message"]
+                    raise Exception(u"Failed to create {} in Salesforce: {}".format(sobject, err))
+            else:
+                raise ValueError("Unsupported operation: {}".format(operation))
+
+    def prepare_salesforce_data(self, sobject, fields, form_input, expr_context):
+        """Collect data for one Salesforce object in the format expected by salesforcebaseconnector
+
+        sobject - API name (developer name) of the sObject in Salesforce
+        fields - mapping from Salesforce field name to an expression for calculating the value
+        request - the request object containing submitted form data
+        expr_context - CMFCore expression context for evaluating TALES expressions
+
+        Several types of expression are supported:
+
+        * "form:name" - Get the value that was entered in the form input called "name"
+        """
+        data = {"type": sobject}
+        for sf_fieldname, value in fields.items():
+            expr = None
+            if value.startswith("form:"):
+                form_fieldname = value[5:]
+                value = form_input.get(form_fieldname)
+            elif value.startswith("path:"):
+                expr = value[5:]
+            elif value.startswith(("python:", "string:")):
+                expr = value
+            if expr is not None:
+                value = get_expression(expr_context, expr, now=DateTime().ISO8601())
+            data[sf_fieldname] = value
+        return data
 
 
 SendToSalesforceAction = ActionFactory(
--- src/jazkarta/easyformplugin/salesforce/interfaces.py
+++ src/jazkarta/easyformplugin/salesforce/interfaces.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 """Module where all interfaces, events and exceptions live."""
 
+import json
+
 from collective.easyform.interfaces.actions import MODIFY_PORTAL_CONTENT, IAction
 from plone.autoform import directives
 from plone.schema.jsonfield import JSONField
@@ -15,9 +17,31 @@ class IJazkartaEasyformpluginSalesforceLayer(IDefaultBrowserLayer):
 class ISaveToSalesforce(IAction):
     """Easyform action which saves data to Salesforce"""
 
-    directives.read_permission(mapping=MODIFY_PORTAL_CONTENT)
-    mapping = JSONField(
-        title=_(u"label_salesforce_mapping", default=u"Field Mapping"),
+    directives.read_permission(operations=MODIFY_PORTAL_CONTENT)
+    operations = JSONField(
+        title=_(u"label_salesforce_operations", default=u"Salesforce Operations"),
         # TODO add description with instructions,
-        defaultFactory=dict,
+        defaultFactory=list,
+        schema=json.dumps({
+            "type": "array",
+            "items": {
+                "type": "object",
+                "required": [
+                    "sobject",
+                    "operation",
+                    "fields",
+                ],
+                "properties": {
+                    "sobject": {"type": "string"},
+                    "operation": {"type": "string", "enum": ["create"]},
+                    "fields": {
+                        "type": "object",
+                        "additionalProperties": {
+                            "type": "string"
+                        }
+                    }
+                },
+                "additionalProperties": False,
+            },
+        })
     )
